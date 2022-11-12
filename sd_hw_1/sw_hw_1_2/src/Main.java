52613ec48f4b78c491d280990f21801871951335
aigulmndzh <60320408+aigulmndzh@users.noreply.github.com>
2022-11-12 12:18:18 +0300

    add homework for the 1st seminar

---
+++ sd_hw_1/sw_hw_1_2/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int num = console.nextInt();
+        System.out.println(fibonacci(num));
+    }
+
+    static long fibonacci (int n) {
+        if (n <= 1)
+            return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
